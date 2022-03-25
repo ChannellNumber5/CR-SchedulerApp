b91eed2031b6a4f5abc5be3eaa1a07ad551d0fc2
Christina R <xsnachannell@gmail.com>
2022-03-24 23:59:41 -0700

    updates README.md with additional resources and updates script.js to display timeblocks on screen, still need to work on code to display timeblock colors correctly and save tasks to localStorage

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 -- [Managing Remote Repositories](https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories)
 
 -- [Moment.js](https://momentjs.com/)
+
+-- [Adding Classes in jquery](https://api.jquery.com/addclass/)
+
+-- [textarea tag in html](https://www.w3schools.com/tags/tag_textarea.asp)
+ P.S. I had no idea that there was a textarea tag. Something new for the books
+
+ -- [Accessing the id attribute](https://stackoverflow.com/questions/3239598/how-can-i-get-the-id-of-an-element-using-jquery)
+
+ -- [Accessing text in element with jquery](https://api.jquery.com/text/)
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const container = $(".container");
 var currentDate = moment().format("dddd MMMM Do, YYYY");
 var currentTime = moment().format("h");
+currentTime = 11;
 var amOrPm = moment().format("A");
+amOrPm = "AM";
 var timeArray = ["9AM","10AM","11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var tasks = ["", "", "", "", "", "", "", "", ""];
 
 //Changes text in p tag to the current date for the when the page is loaded
 $("#currentDay").text(currentDate);
@@ -10,21 +14,54 @@ console.log(currentDate);
 
 for (let i = 0; i < timeArray.length; i++) {
     var newDiv = $("<div>");
-    var hour = $("<div>");
-    hour.attr("class", "hour");
+    newDiv.attr("class", "time-block row" );
     newDiv.attr("id", timeArray[i]);
+    var hourDiv = $("<div>");
+    hourDiv.attr("class", "hour col-1");
+    hourDiv.text(timeArray[i]);
+    newDiv.append(hourDiv);
+    var desBlock = $("<textarea>");
+    desBlock.attr("class", "description col-10");
+    newDiv.append(desBlock);
+    var saveBtn = $("<button>");
+    saveBtn.attr("class", "saveBtn col-1");
+    newDiv.append(saveBtn);
+    
     if (timeArray[i] === currentTime + amOrPm) {
-        newDiv.attr("class", "time-block present");
-    } else if (i > 2 && amOrPm === "AM") {
-        newDiv.attr("class", "time-block future");
+        desBlock.addClass("present");
+    } else if (currentTime > 5 && amOrPm == "PM") {
+        desBlock.addClass("past");
+    } else if (currentTime < 9 && amOrPm == "AM") {
+        desBlock.addClass("future");
+    } else if (i >= 1 && amOrPm === "AM") {
+        desBlock.addClass("future");
     } else {
-        newDiv.attr("class", "time-block past");
+        desBlock.addClass("past");
+    }
+    container.append(newDiv);
+}
+
+container.on('click', function(event) {
+    var clickedButton = $(event.target);
+    var hourEl = clickedButton.parent();
+    for (let i=0; i < timeArray.length; i++) {
+        if (hourEl.id == timeArray[i]) {
+            tasks[i] = hourEl.children(1).text();
+        }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));    
+})
+
+function loadTasks() {
+    if (JSON.parse(localStorage.getItem("tasks")) == null){
+        return;
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+        for (let i = 0; i < tasks.length; i++){
+            console.log(container.children().children(i));
+            // container.children(i).children(2).text(tasks[i]);
+        }
     }
 }
-var nineAM = $("<div>");
-nineAM.attr({"id", "9AM",})
-if (nineAM.id === currentTime) {
-    nineAM.attr("class","time-blockpresent");
-} else if (nineAM.id ) {
-    nineAM.attr("class", "")
-}
+
+loadTasks();
